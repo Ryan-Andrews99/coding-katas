486fe498d368f0a87c73febf8b1b59dfb78e7627
Ryan-Andrews99 <andrewsr99@gmail.com>
2023-12-02 23:06:58 +0000

    add comment about why part 2 is awful

--- src/adventOfCode2023/day1/index.test.ts
+++ src/adventOfCode2023/day1/index.test.ts
@@ -87,7 +87,7 @@ describe("replaceWordNumbersWithDigits tests", () => {
         Object.keys(wordsToNums),
         wordsToNums,
       ),
-    ).toEqual("82");
+    ).toEqual("82"); // hate this test 
   });
 });
 
