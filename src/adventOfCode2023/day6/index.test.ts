d37f9fe0c06bd62a8d05c0f5dcf7fe230f5d0d69
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2023-12-10 23:13:18 +0000

    day 6

---
+++ src/adventOfCode2023/day6/index.test.ts
@@ -0,0 +1,32 @@
+import { calculateDistance, calculateRaces, returnAnswerPart1, returnAnswerPart2 } from './index'
+
+describe('calculateDistance tests', () => {
+    it('calculates the distance that can be traveled based on speed and time remaning', () => {
+        expect(calculateDistance(1,6)).toEqual(6)
+    })
+
+    it('calculates the distance that can be traveled based on speed and time remaning', () => {
+        expect(calculateDistance(2,5)).toEqual(10)
+    })
+})
+
+describe('calculateRace tests ', () => {
+    it('calculates all race outcomes for a given time and distance', () => {
+        expect(calculateRaces(7)).toEqual([0, 6, 10, 12, 12, 10 , 6, 0])
+    })
+})
+
+
+describe('returnAnswerPart1', () => {
+    it('passes the test data', () => {
+        expect(returnAnswerPart1(`Time:      7  15   30
+        Distance:  9  40  200`)).toEqual(288)
+    })
+})
+
+describe('returnAnswerPart2', () => {
+    it('passes the test data', () => {
+        expect(returnAnswerPart2(`Time:      7  15   30
+        Distance:  9  40  200`)).toEqual(71503)
+    })
+})
---
+++ src/adventOfCode2023/day6/index.ts
@@ -0,0 +1,53 @@
+import { readTxtFile } from "../util/readTxtFile";
+
+export const calculateDistance = (
+  speed: number,
+  timeRemaining: number
+): number => speed * timeRemaining;
+
+export const calculateRaces = (totalRaceTime: number) =>
+  Array(totalRaceTime + 1)
+    .fill(0)
+    .map((_, index) => index * (totalRaceTime - index));
+
+export const returnAnswerPart1 = (input: string) => {
+  const [times, distances] = input
+    .split("\n")
+    .map((line) => line.trim())
+    .map((line) =>
+      line
+        .split(" ")
+        .filter((char) => !isNaN(Number(char.trim())) && char.length > 0)
+        .map((char) => Number(char))
+    );
+
+  return times
+    .map(
+      (time, index) =>
+        calculateRaces(time).filter((distance) => distance > distances[index])
+          .length
+    )
+    .reduce((product, margin) => (product *= margin), 1);
+}
+
+const input = readTxtFile('src/adventOfCode2023/day6/input.txt')
+
+console.log(returnAnswerPart1(input))
+
+export const returnAnswerPart2 = (input: string) => {
+    const [time, distance] = input
+      .split("\n")
+      .map((line) => line.trim())
+      .map((line) =>
+        line
+          .split(" ")
+          .filter((char) => !isNaN(Number(char.trim())) && char.length > 0)
+          .join('')
+      ).map(num => Number(num))
+  
+    return calculateRaces(time).filter(
+        dist => dist > distance
+    ).length
+  }
+
+  console.log(returnAnswerPart2(input))
