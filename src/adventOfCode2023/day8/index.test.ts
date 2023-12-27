b24042b20c8b7e2741be6c381fc9c163e9391870
Ryan-Andrews99 <andrewsr99@gmail.com>
2023-12-27 22:25:31 +0000

    day 8

---
+++ src/adventOfCode2023/day8/index.test.ts
@@ -0,0 +1,121 @@
+import { findNode, findNextNode, parseInput, traverseNodes, part1NodeMatch, greatestCommonDivisor, lowestCommonMultiple } from "./index";
+
+describe("parseInput tests", () => {
+  it("parses the input correctly", () => {
+    expect(
+      parseInput(`RL
+
+        AAA = (BBB, CCC)
+        BBB = (DDD, EEE)
+        CCC = (ZZZ, GGG)
+        DDD = (DDD, DDD)
+        EEE = (EEE, EEE)
+        GGG = (GGG, GGG)
+        ZZZ = (ZZZ, ZZZ)`),
+    ).toStrictEqual([
+      ["R", "L"],
+      [
+        ["AAA", "BBB", "CCC"],
+        ["BBB", "DDD", "EEE"],
+        ["CCC", "ZZZ", "GGG"],
+        ["DDD", "DDD", "DDD"],
+        ["EEE", "EEE", "EEE"],
+        ["GGG", "GGG", "GGG"],
+        ["ZZZ", "ZZZ", "ZZZ"],
+      ],
+    ]);
+  });
+});
+
+describe("findNode tests", () => {
+  it("finds a given node", () => {
+    expect(
+      findNode("AAA", [
+        ["AAA", "BBB", "CCC"],
+        ["BBB", "DDD", "EEE"],
+        ["CCC", "ZZZ", "GGG"],
+        ["DDD", "DDD", "DDD"],
+        ["EEE", "EEE", "EEE"],
+        ["GGG", "GGG", "GGG"],
+        ["ZZZ", "ZZZ", "ZZZ"],
+      ]),
+    ).toStrictEqual(["AAA", "BBB", "CCC"]);
+  });
+
+  it("throws an error for missing node", () => {
+    expect(() =>
+      findNode("FFF", [
+        ["AAA", "BBB", "CCC"],
+        ["BBB", "DDD", "EEE"],
+        ["CCC", "ZZZ", "GGG"],
+        ["DDD", "DDD", "DDD"],
+        ["EEE", "EEE", "EEE"],
+        ["GGG", "GGG", "GGG"],
+        ["ZZZ", "ZZZ", "ZZZ"],
+      ]),
+    ).toThrowError("Invalid number of nodes returned: 0");
+  });
+
+  it("throws for duplicate nodes", () => {
+    expect(() =>
+      findNode("AAA", [
+        ["AAA", "BBB", "CCC"],
+        ["AAA", "DDD", "EEE"],
+        ["CCC", "ZZZ", "GGG"],
+        ["DDD", "DDD", "DDD"],
+        ["EEE", "EEE", "EEE"],
+        ["GGG", "GGG", "GGG"],
+        ["ZZZ", "ZZZ", "ZZZ"],
+      ]),
+    ).toThrowError("Invalid number of nodes returned: 2");
+  });
+});
+
+describe("nextNode tests", () => {
+  it('finds the next node given a direction and a current node', () => {
+    expect(findNextNode(  ["AAA", "BBB", "CCC"], 'L')).toStrictEqual('BBB')
+  })
+});
+
+
+describe("traverseNodes tests", () => {
+  it("traverses the nodes until ZZZ is the current node",  () => {
+    expect(
+      traverseNodes(
+        ["R", "L"],
+        [
+          ["AAA", "BBB", "CCC"],
+          ["BBB", "DDD", "EEE"],
+          ["CCC", "ZZZ", "GGG"],
+          ["DDD", "DDD", "DDD"],
+          ["EEE", "EEE", "EEE"],
+          ["GGG", "GGG", "GGG"],
+          ["ZZZ", "ZZZ", "ZZZ"],
+        ], part1NodeMatch
+      ),
+    ).toStrictEqual(2);
+  });
+});
+
+describe("gcd tests", () => {
+  it("finds the gcd",  () => {
+    expect(
+      greatestCommonDivisor(
+        54, 24
+      ),
+    ).toStrictEqual(6);
+  });
+});
+
+
+describe("lcm tests", () => {
+  it("lcm",  () => {
+    expect(
+      lowestCommonMultiple(
+        12, 15
+      ),
+    ).toStrictEqual(60);
+  });
+});
+
+
---
+++ src/adventOfCode2023/day8/index.ts
@@ -0,0 +1,84 @@
+import { readTxtFile } from "../util/readTxtFile";
+
+export const parseInput = (input: string): [string[], string[][]] =>
+  input
+    .split("\n")
+    .filter((line) => line.length > 0)
+    .map((line) => line.trim())
+    .reduce(
+      (parsedInput: [string[], string[][]], line, index) => {
+        if (index === 0) {
+          return [line.split(""), [...parsedInput[1]]];
+        } else
+          return [
+            parsedInput[0],
+            [
+              ...parsedInput[1],
+              line
+                .split("=")
+                .flatMap((el) => el.replaceAll(/[()\s]/g, "").split(",")),
+            ],
+          ];
+      },
+      [[], []],
+    );
+
+export const findNode = (nodeToFind: string, nodes: string[][]): string[] => {
+  const matchingNode = nodes.filter((node) => node[0] === nodeToFind);
+  if (matchingNode.length !== 1) {
+    throw new Error(
+      `Invalid number of nodes returned: ${matchingNode.length}, node: ${nodeToFind}`,
+    );
+  }
+  return matchingNode[0];
+};
+
+
+export const findNextNode = (currentNode: string[], direction: string) => direction === 'L' ? currentNode[1] : currentNode[2]
+
+export const part1NodeMatch = (node: string): boolean => node === 'ZZZ'
+
+export const traverseNodes = (
+  directions: string[],
+  nodes: string[][],
+  matchCondition: (node: string) => boolean,
+  currentNode = "AAA",
+  index = 0,
+  steps = 0,
+): number => {
+  if (index > directions.length - 1) {
+    index = 0;
+  }
+  const nextNode = findNextNode(findNode(currentNode, nodes), directions[index])
+  if (nextNode === currentNode) {
+    throw new Error("AAAA recursive hell :)");
+  }
+  index++;
+  steps++;
+  if (matchCondition(nextNode)) { //base case
+    return steps;
+  } else return traverseNodes(directions, nodes, matchCondition, nextNode, index, steps);
+};
+
+
+const [directions, nodes] = parseInput(
+  readTxtFile("src/adventOfCode2023/day8/input.txt"),
+);
+
+//to run this you need to increase the max stack size in node 
+// node --stack-size=15000 -r ts-node/register src/adventOfCode2023/day8/index.ts
+let start = performance.now();
+console.log(traverseNodes(directions, nodes, part1NodeMatch)) //11911
+let end = performance.now();
+console.log('part 1 took', end - start) //approx 150ms
+
+//part 2
+export const greatestCommonDivisor = (a: number, b: number): number => a ? greatestCommonDivisor(b % a, a) : b;
+export const lowestCommonMultiple = (a: number, b: number): number => a * b / greatestCommonDivisor(a, b);
+const nodesEndingA = nodes.filter(node => node[0][2] === 'A')
+const part2NodeMatch = (node: string): boolean => node[2] === 'Z'
+
+start = performance.now();
+console.log(nodesEndingA.map(node => traverseNodes(directions, nodes, part2NodeMatch, node[0])).reduce(lowestCommonMultiple)) //10151663816849
+end = performance.now();
+console.log('part 2 took', end - start) //part 2 took 625.1938680000603
