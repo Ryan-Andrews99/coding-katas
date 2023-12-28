52c0a12e99cbf64ff6f9ef3fd8374284f8309152
Ryan-Andrews99 <andrewsr99@gmail.com>
2023-12-28 23:10:56 +0000

    day9

---
+++ src/adventOfCode2023/day9/index.test.ts
@@ -0,0 +1,153 @@
+import {
+  calcDiffs,
+  createRanges,
+  extrapolateBackwards,
+  extrapolateForward,
+  parseInput,
+  part1,
+  part2,
+  returnHistory,
+} from "./index";
+
+describe("createRanges tests", () => {
+  it("creates the ranges for test data", () => {
+    expect(createRanges([0, 3, 6, 9, 12, 15])).toStrictEqual([
+      [0, 3, 6, 9, 12, 15],
+      [3, 3, 3, 3, 3],
+      [0, 0, 0, 0],
+    ]);
+  });
+
+  it("creates the ranges for test data", () => {
+    expect(createRanges([10, 13, 16, 21, 30, 45])).toStrictEqual([
+      [10, 13, 16, 21, 30, 45],
+      [3, 3, 5, 9, 15],
+      [0, 2, 4, 6],
+      [2, 2, 2],
+      [0, 0],
+    ]);
+  });
+});
+
+describe("calculate differences", () => {
+  it("returns an array of differences between numbers", () => {
+    expect(calcDiffs([0, 3, 6, 9, 12, 15])).toStrictEqual([3, 3, 3, 3, 3]);
+  });
+
+  it("returns an array of differences between numbers", () => {
+    expect(calcDiffs([3, 3, 3, 3, 3])).toStrictEqual([0, 0, 0, 0]);
+  });
+});
+
+describe("extrapolateForward", () => {
+  it("extrapolates correctly", () => {
+    expect(
+      extrapolateForward([
+        [0, 3, 6, 9, 12, 15],
+        [3, 3, 3, 3, 3],
+        [0, 0, 0, 0],
+      ]),
+    ).toStrictEqual([
+      [0, 3, 6, 9, 12, 15, 18],
+      [3, 3, 3, 3, 3, 3],
+      [0, 0, 0, 0, 0],
+    ]);
+  });
+
+  it("extrapolates test data correctly", () => {
+    expect(
+      extrapolateForward([
+        [10, 13, 16, 21, 30, 45],
+        [3, 3, 5, 9, 15],
+        [0, 2, 4, 6],
+        [2, 2, 2],
+        [0, 0],
+      ]),
+    ).toStrictEqual([
+      [10, 13, 16, 21, 30, 45, 68],
+      [3, 3, 5, 9, 15, 23],
+      [0, 2, 4, 6, 8],
+      [2, 2, 2, 2],
+      [0, 0, 0],
+    ]);
+  });
+});
+
+describe("returnHistory", () => {
+  it("returns last val of index 0", () => {
+    expect(
+      returnHistory([
+        [0, 3, 6, 9, 12, 15, 18],
+        [3, 3, 3, 3, 3, 3],
+        [0, 0, 0, 0, 0],
+      ]),
+    ).toStrictEqual(18);
+  });
+
+  it("returns last val of index 0", () => {
+    expect(
+      returnHistory([
+        [10, 13, 16, 21, 30, 45, 68],
+        [3, 3, 5, 9, 15, 23],
+        [0, 2, 4, 6, 8],
+        [2, 2, 2, 2],
+        [0, 0, 0],
+      ]),
+    ).toStrictEqual(68);
+  });
+});
+
+describe("parseInput tests", () => {
+  it("parses input correctly", () => {
+    expect(
+      parseInput(`0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45`),
+    ).toStrictEqual([
+      [0, 3, 6, 9, 12, 15],
+      [1, 3, 6, 10, 15, 21],
+      [10, 13, 16, 21, 30, 45],
+    ]);
+  });
+});
+
+describe("part1 tests", () => {
+  it("passes test data", () => {
+    expect(
+      part1(`0 3 6 9 12 15
+          1 3 6 10 15 21
+          10 13 16 21 30 45`),
+    ).toEqual(114);
+  });
+});
+
+describe("extrapolateBackwards", () => {
+  
+    it("extrapolates test data correctly", () => {
+      expect(
+        extrapolateBackwards([
+          [10, 13, 16, 21, 30, 45],
+          [3, 3, 5, 9, 15],
+          [0, 2, 4, 6],
+          [2, 2, 2],
+          [0, 0],
+        ]),
+      ).toStrictEqual([
+        [5, 10, 13, 16, 21, 30, 45],
+        [5, 3, 3, 5, 9, 15],
+        [-2, 0, 2, 4, 6],
+        [2, 2, 2, 2],
+        [0, 0, 0],
+      ]);
+    });
+  });
+  
+  describe("part2 tests", () => {
+    it("passes test data", () => {
+      expect(
+        part2(`0 3 6 9 12 15
+            1 3 6 10 15 21
+            10 13 16 21 30 45`),
+      ).toEqual(2);
+    });
+  });
---
+++ src/adventOfCode2023/day9/index.ts
@@ -0,0 +1,88 @@
+import { readTxtFile } from "../util/readTxtFile";
+
+export const createRanges = (initialRange: number[]): number[][] => {
+  const ranges = [initialRange];
+  // I did not extract the last val out to a constant as it
+  // needs to reevaluated at each run of the while loop
+  while (!ranges[ranges.length - 1].every((val) => val === 0)) {
+    ranges.push(calcDiffs(ranges[ranges.length - 1]));
+  }
+  return ranges;
+};
+export const calcDiffs = (nums: number[]) =>
+  nums.reduce((acc: number[], num, index, arr) => {
+    if (index === arr.length - 1) {
+      return acc;
+    } else return [...acc, arr[index + 1] - num];
+  }, []);
+
+export const extrapolateForward = (vals: number[][]) =>
+  vals
+    .reverse()
+    .reduce((vals: number[][], row, index) => {
+      if (index === 0) {
+        return [...vals, [...row, 0]];
+      } else {
+        const prevRow = vals[index - 1];
+        const lastValOfPrevRow = prevRow[prevRow.length - 1];
+        return [...vals, [...row, row[row.length - 1] + lastValOfPrevRow]];
+      }
+    }, [])
+    .reverse();
+
+export const returnHistory = (vals: number[][]): number =>
+  vals[0][vals[0].length - 1];
+
+export const parseInput = (input: string): number[][] =>
+  input.split("\n").map((line) =>
+    line
+      .split(" ")
+      .filter((char) => char.length > 0)
+      .map((char) => Number(char.trim())),
+  );
+
+export const part1 = (input: string): number =>
+  parseInput(input)
+    .map(createRanges)
+    .map(extrapolateForward)
+    .map(returnHistory)
+    .reduce((sum, val) => (sum += val), 0);
+
+const input = readTxtFile('src/adventOfCode2023/day9/input.txt')
+
+
+let start = performance.now();
+console.log(part1(input)) // 1762065988
+let end = performance.now();
+console.log("part 1 took", end - start); //part 1 took 48.73225999996066
+
+
+export const extrapolateBackwards = (vals: number[][]) =>
+  vals
+    .reverse()
+    .reduce((vals: number[][], row, index) => {
+      if (index === 0) {
+        return [...vals, [0, ...row]];
+      } else {
+        const prevRow = vals[index - 1];
+        const firstValOfPrevRow = prevRow[0];
+        return [...vals, [ row[0] - firstValOfPrevRow, ...row,]];
+      }
+    }, [])
+    .reverse();
+
+export const returnFirstHistory = (vals: number[][]): number =>
+  vals[0][0];
+
+
+export const part2 = (input: string): number =>
+parseInput(input)
+  .map(createRanges)
+  .map(extrapolateBackwards)
+  .map(returnFirstHistory)
+  .reduce((sum, val) => (sum += val), 0);
+
+start = performance.now();
+console.log(part2(input)) // 1066
+end = performance.now();
+console.log("part 2 took", end - start) //part 2 took 21.737158998847008
